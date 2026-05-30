0341a4a656ac48e41827e7f3b9829d7e8f5a5f4e
Lakshmi Shivani <boyinalakshmishivani@gmail.com>
2026-05-30 15:42:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector(".worker-btn")
+.addEventListener("click",()=>{
+
+alert(
+"Worker Registration Page will open here."
+);
+
+});
+
+document.querySelector(".contractor-btn")
+.addEventListener("click",()=>{
+
+alert(
+"Contractor Registration Page will open here."
+);
+
+});
