955c0c10229b6218ab336136359e6e337f1c1c75
Lakshmi Shivani <boyinalakshmishivani@gmail.com>
2026-05-30 17:41:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function login() {
+
+    alert("Login Successful!");
+
+    window.location.href = "role-selection.html";
+
+}
+
+function registerWorker() {
+
+    alert("Worker Registration Successful!");
+
+    window.location.href = "worker-dashboard.html";
+
+}
+
+function registerContractor() {
+
+    alert("Contractor Registration Successful!");
+
+    window.location.href = "contractor-dashboard.html";
+
+}
+
+function applyJob() {
+
+    alert("Application Submitted Successfully!");
+
+}
+
+function assignWorker() {
+
+    alert("Worker Assigned Successfully!");
+
+}
+
+function goToJobs() {
+
+    window.location.href = "jobs.html";
+
+}
+
+function goToAI() {
+
+    window.location.href = "ai-recommendation.html";
+
+}
+
+function createJob() {
+
+    alert("Job Request Created Successfully!");
+
+}
